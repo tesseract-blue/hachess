dd5f2a47bf34c83fd59c0707ea305db4e473cecf
Adam Atbi <adamatbi@gmail.com>
2022-07-26 19:44:20 -0700

    add Adam_SimpleMiniMax

--- hachess/agents/Adam_SimpleMiniMax/agent.py
+++ hachess/agents/Adam_SimpleMiniMax/agent.py
@@ -2,20 +2,18 @@
 hachess simulation expects the following methods:
 - decide
 
-
-The agent randomly selects a move from the list of legal moves.
-
 """
 import os
-from copy import deepcopy
-import random
+import time
 import chess
-
+from operator import itemgetter
 
 class Agent:
     def __init__(self) -> None:
         self.__init_agent_resources_path()
         self.__init_memory()
+        self.__num_evaluated = 0
+
 
     def __init_agent_resources_path(self) -> None:
         """
@@ -32,18 +30,70 @@ class Agent:
         self.__memory = list()
 
     def decide(self, board: chess.Board) -> chess.Move:
-        return self.minimax(board, 2, True)
+        self.__num_evaluated = 0
+        return self.minimax(board, None,3,board.turn, True)[0]
 
-    def minimax(self, board: chess.Board, depth: int, self_move: bool) -> chess.Move:
+    def minimax(self, board: chess.Board,move: chess.Move, depth: int, self_white: bool, self_move: bool) -> tuple[chess.Move,float]:
         """evaluates position using minimax tree search."""
-        for i in list(board.legal_moves):
-            if self_move:
-                # agent has move
-                if depth == 0:
-                    # return choice based on evaluation function
-                    pass
+        if move!= None:
+            #push move to board
+            board.push(move)
 
-        return list(board.legal_moves)[0]
+        if depth == 0:
+            #tree leaf reached. return evaluation of board position
+            if move != None:
+                board.pop()
 
-    def evaluate_position(self, board: chess.Board, self_move: bool) -> float:
-        return 0.0
+            return (None,self.evaluate_position(board,self_white))
+        
+        if board.outcome() != None:
+            #check game is unresolved and return game result scores if resolved.
+            result =  (None,self.resolution_score(board.result(),self_white))
+            if move != None:
+                board.pop()
+            return result
+
+        branches = {(legal_move,self.minimax(board,legal_move,depth-1,self_white,not self_move)[1]) for legal_move in board.legal_moves}
+
+        if move != None:
+                board.pop()
+        
+        if self_move:
+            result = max(branches,key=itemgetter(1))
+            return result
+        else:
+            result = min(branches,key=itemgetter(1))
+            return result
+
+    def evaluate_position(self, board: chess.Board,self_white:bool) -> float:
+        """
+        simple evaluation function using piece values. evaluates from side of player to move
+        returns positive for agent advantage and negative for opponent advantage.
+        
+        """
+        self.__num_evaluated+=1
+
+        if board.outcome() != None:
+            #check game is unresolved and return game result scores if resolved.
+            return self.resolution_score(board.result(),self_white)
+
+        score=len(board.pieces(chess.PAWN,chess.WHITE))
+        score+=3*len(board.pieces(chess.BISHOP,chess.WHITE))
+        score+=3*len(board.pieces(chess.KNIGHT,chess.WHITE))
+        score+=5*len(board.pieces(chess.ROOK,chess.WHITE))
+        score+=9*len(board.pieces(chess.QUEEN,chess.WHITE))
+        score-=len(board.pieces(chess.PAWN,chess.BLACK))
+        score-=3*len(board.pieces(chess.BISHOP,chess.BLACK))
+        score-=3*len(board.pieces(chess.KNIGHT,chess.BLACK))
+        score-=5*len(board.pieces(chess.ROOK,chess.BLACK))
+        score-=9*len(board.pieces(chess.QUEEN,chess.BLACK))
+        return score
+
+    def resolution_score(self, board_result: str,self_white:bool) -> float:
+        if board_result == "0-0":
+            return 0
+        elif board_result == "1-0":
+            return 1000000 if self_white else -1000000
+        else:
+            return -1000000 if self_white else 1000000
+            
